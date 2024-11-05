3c3350464a65849072d3bcd37ac098aa94fdc41d
Jungmin Moon <jmmoon96@gmail.com>
2024-11-05 02:27:43 -0500

    First commit

---
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/FfxiiJobFiestaApplication.java
@@ -0,0 +1,13 @@
+package com.zodiacfiesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfxiiJobFiestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfxiiJobFiestaApplication.class, args);
+	}
+
+}
---
+++ FFXII-Job-Fiesta/src/test/java/com/zodiacfiesta/FfxiiJobFiestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zodiacfiesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfxiiJobFiestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
