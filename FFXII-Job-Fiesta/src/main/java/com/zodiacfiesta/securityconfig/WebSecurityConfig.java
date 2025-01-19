4291f6985cf62073811a1970ee6c8b03fc646901
Jungmin Moon <jmmoon96@gmail.com>
2025-01-19 03:30:44 -0500

    Finished WebConfig for now, need to design the html

--- FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/securityconfig/WebSecurityConfig.java
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/securityconfig/WebSecurityConfig.java
@@ -3,10 +3,12 @@ package com.zodiacfiesta.securityconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.zodiacfiesta.repositories.UserRepository;
@@ -39,5 +41,18 @@ public class WebSecurityConfig implements WebMvcConfigurer{
 		return authProvider;
 	}
 	
-	
+	@Bean
+	SecurityFilterChain config(HttpSecurity http) throws Exception {
+		http.authenticationProvider(authenticationProvider());
+		
+		http.authorizeHttpRequests(a -> a.requestMatchers("/", "/home", "/login", "/register").permitAll());
+		
+		http.formLogin(l -> l.loginPage("/login"));
+		http.logout((logout) -> logout.logoutSuccessUrl("/home")
+										.logoutUrl("/logout")
+										.invalidateHttpSession(true)
+										.deleteCookies("JESSIONID"));
+		
+		return http.build();
+	}
 }
