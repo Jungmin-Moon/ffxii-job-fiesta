40fdfe5a2ab88095fe89a8b7bf17f6b972b4d26f
Jungmin Moon <jmmoon96@gmail.com>
2025-01-19 01:52:11 -0500

    Work on configuring UserDetails and WebSecurityConfig

--- FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/securityconfig/WebSecurityConfig.java
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/securityconfig/WebSecurityConfig.java
@@ -2,17 +2,41 @@ package com.zodiacfiesta.securityconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.zodiacfiesta.repositories.UserRepository;
+import com.zodiacfiesta.services.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig implements WebMvcConfigurer{
-
+	
+	private UserRepository userRepo;
+	
 	@Bean
-	UserDetailsService userDetailsService() {
-		return new UserService();
+	UserDetailsService userDetailsService(UserRepository userRepo) {
+		return new UserService(userRepo);
+	}
+	
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		
+		authProvider.setUserDetailsService(userDetailsService(userRepo));
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
 	}
 	
 	
--- FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/services/UserService.java
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/services/UserService.java
@@ -1,8 +1,54 @@
 package com.zodiacfiesta.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.zodiacfiesta.entities.CustomUserDetails;
+import com.zodiacfiesta.entities.User;
+import com.zodiacfiesta.repositories.UserRepository;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
+	private UserRepository userRepo;
+	
+	public UserService(UserRepository userRepo) {
+		this.userRepo = userRepo;	
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.getByUsername(username);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not find user.");
+		}
+		
+		return new CustomUserDetails(user);
+	}
+	
+	public boolean createUser(User user) {
+		boolean created = true;
+		
+		String checkUsername = user.getUsername();
+		
+		if (userRepo.getByUsername(checkUsername) == null) {
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			
+			String encodedPassword = passwordEncoder.encode(user.getPassword());
+			
+			user.setPassword(encodedPassword);
+			user.setRole("USER");
+			
+			userRepo.save(user);
+			
+		} else {
+			created = false;
+		}
+		
+		return created;
+	}
 }
