58f817c3245ca27b9ec26bf28ce86fe7bd1f452d
Jungmin Moon <jmmoon96@gmail.com>
2025-01-27 01:58:49 -0500

    Changed the reutrn types of the jobs generator methods to Lists intead.
    Lists have access to more methods to make it easy to check for dupes.

--- FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/services/JobGenerator.java
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/services/JobGenerator.java
@@ -1,49 +1,78 @@
 package com.zodiacfiesta.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 
 import com.zodiacfiesta.entities.Jobs;
-
-import java.util.Random;
 
 //used by RunGenerator to generate Jobs for the run type sent by the user to the controller
 @Service
 public class JobGenerator {
 	
 	//Random random = new Random();
-	
+	Jobs[] jobs = Jobs.values();
 	//Jobs[] jobs = Jobs.values();
 	//jobs[new Random().nextInt(jobs.length)];
 	
 	//order for jobs should be Vaan, Fran/Balthier, Basch, Ashe, Penelo
 	
 	//for every character to have a different job1
-	public Jobs[] singleDifferentJobs() {
+	public List<Jobs> singleDifferentJobs() {
+		ArrayList<Jobs> selected = new ArrayList<>();
 		
+		for (int i = 0; i < 6; i++) {
+			Jobs temp = jobs[new Random().nextInt(jobs.length)];
+			
+			if (selected.contains(temp)) {
+				continue;
+			} else {
+				selected.add(temp);
+			}
+		}
+			
+		return selected;
 	}
 	
 	//every character getting the same job1 
 	public Jobs oneJobEveryone() {
+		Jobs selectedOne = jobs[new Random().nextInt(jobs.length)];
 		
+		return selectedOne;
 	}
 	
 	//for every character getting the same job1, and same job2
-	public Jobs[] oneJobEveryoneBoth() {
+	public List<Jobs> oneJobEveryoneBoth() {
+		ArrayList<Jobs> bothJobs = new ArrayList<>();
 		
+		for (int i = 0; i < 2; i++) {
+			Jobs temp = jobs[new Random().nextInt(jobs.length)];
+			
+			if (bothJobs.contains(temp)) {
+				continue;
+			} else {
+				bothJobs.add(temp);
+			}
+		}
+		
+		
+		return bothJobs;
 	}
 	
 	//for every character having a different job1 and job2
-	public Jobs[][] differentBothJobs() {
+	public List<List<Jobs>> differentBothJobs() {
 		
 	}
 	
 	//every character has different job1s but same Job2
-	public Jobs[][] differentJobTwo() {
+	public List<List<Jobs>> differentJobTwo() {
 		
 	}
 	
 	//every character different job2s but same Job1
-	public Jobs[][] differentJobOne() {
+	public List<List<Jobs>> differentJobOne() {
 		
 	}
 }
