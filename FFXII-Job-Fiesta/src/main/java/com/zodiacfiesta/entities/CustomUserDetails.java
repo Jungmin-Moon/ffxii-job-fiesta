bfccb17b5ff8001f86a389d3e9dce8e003c83566
Jungmin Moon <jmmoon96@gmail.com>
2025-02-02 23:27:39 -0500

    Created better comments for the User and CustomUserDetails classes

--- FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/entities/CustomUserDetails.java
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/entities/CustomUserDetails.java
@@ -7,16 +7,30 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+/*
+ * A {@code CustomUserDetails} object that represents the UserDetails of the user
+ */
+
 public class CustomUserDetails implements UserDetails{
 	
 	private static final long serialVersionUID = 1L;
 	
 	private User user;
 	
+	/*
+	 * Constructor for CustomUserDetails
+	 * @param user: a User user object to store into the user field
+	 * @return: returns nothing
+	 */
 	public CustomUserDetails(User user) {
 		this.user = user;
 	}
 	
+	/*
+	 * Sets the authority of the user when they login
+	 * @param: no parameters
+	 * @return: returns the list of authorities the user has
+	 */
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole());
@@ -24,11 +38,21 @@ public class CustomUserDetails implements UserDetails{
 		return Arrays.asList(authority);
 	}
 	
+	/*
+	 * Gets the user's password
+	 * @param: no parameters
+	 * @return: no returns
+	 */
 	@Override
 	public String getPassword() {
 		return user.getPassword();
 	}
 	
+	/*
+	 * Gets the username of the user
+	 * @param: no parameters
+	 * @return: no returns
+	 */
 	@Override
 	public String getUsername() {
 		return user.getUsername();
--- FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/entities/User.java
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/entities/User.java
@@ -6,6 +6,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+/*
+ * A {@code User} object represents a user and their credentials after registering an account with the system.
+ * with various credentials such as username which is public knowledge, but id, password, role, firstName and lastName
+ * will not be made public information. 
+ */
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -20,40 +26,110 @@ public class User {
 	private String firstName;
 	private String lastName;
 	
-	
+	/*
+	 * Fetches the id of the user from the db
+	 * @param: no parameters
+	 * @return: the user id
+	 */
 	public long getId() {
 		return id;
 	}
+	
+	/*
+	 * Sets the user id
+	 * @param id: the id to store for this particular user
+	 * @return: no return
+	 */
 	public void setId(long id) {
 		this.id = id;
 	}
+	
+	/*
+	 * Fetches the username of the user from the db
+	 * @param: no parameters
+	 * @return: the username of the user
+	 */
 	public String getUsername() {
 		return username;
 	}
+	
+	/*
+	 * Sets the username for this user
+	 * @param username: the username to store into the field value for this value
+	 * @return: no return
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	
+	/*
+	 * Fetches the user password from the db
+	 * @param: no parameters
+	 * @return: the user's password
+	 */
 	public String getPassword() {
 		return password;
 	}
+	
+	/*
+	 * Sets the user's password
+	 * @param password: the password to store into the field password
+	 * @return: no returns
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	/*
+	 * Fetches the user's role that is stored in the db
+	 * @param: no parameters
+	 * @return: the user's role
+	 */
 	public String getRole() {
 		return role;
 	}
+	
+	/*
+	 * Sets the user's role
+	 * @param role: the role to store into the field value role
+	 * @return: no returns
+	 */
 	public void setRole(String role) {
 		this.role = role;
 	}
+	
+	/*
+	 * Fetches the user's firstName from the db
+	 * @param: no parameters
+	 * @return: the user's firstName 
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
+	
+	/*
+	 * Sets the user's firstName
+	 * @param firstName: the String to store into the firstName
+	 * @return: no returns
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
+	/*
+	 * Fetches the user's lastName from the db
+	 * @param: no parameters
+	 * @return: lastName of the user 
+	 */
 	public String getLastName() {
 		return lastName;
 	}
+	
+	/*
+	 * Sets the user's lastName
+	 * @param lastName: the String to store into the lastName
+	 * @return: no returns
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
