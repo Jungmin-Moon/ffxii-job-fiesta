07aaf14a8d28c828a4a84471ed24d256f7767d09
Jungmin Moon <jmmoon96@gmail.com>
2025-02-03 23:16:16 -0500

    Completion of the JobGenerator class with comments.

--- FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/services/JobGenerator.java
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/services/JobGenerator.java
@@ -10,28 +10,29 @@ import com.zodiacfiesta.entities.Jobs;
 
 /*
  * Class useds to generate the jobs for the player depending on the type of run they request in RunGenerator
+ * The Jobs in the list will be added in terms of when the party member permanently joins the party.
+ * Vaan, Fran Balthier (for purposes Fran will be listed first then Baltier), Basch, Ashe, Penelo
+ * (0,0) and (1,0) will be the jobs for Vaan and so on
  */
 @Service
 public class JobGenerator {
-	
-	//Random random = new Random();
+
 	Jobs[] jobs = Jobs.values();
 	//Jobs[] jobs = Jobs.values();
 	//jobs[new Random().nextInt(jobs.length)];
 	
 	//order for jobs should be Vaan, Fran/Balthier, Basch, Ashe, Penelo
 	
-	//methods for generating
-	
-
 	/*
-	 * Used to generate a List<Jobs> of six unique jobs
+	 * Used to generate a List<Jobs> of six unique jobs Job1(Unique)/(No Job2)
 	 * @param: no parameters
 	 * @return selected: list of jobs with no duplicates to be returned
 	 */
 	public List<Jobs> singleDifferentJobs() {
+		//One-Dimensional list to add jobs
 		ArrayList<Jobs> selected = new ArrayList<>();
 		
+		//Loop that only loops 6 times total
 		for (int i = 0; i < 6; i++) {
 			addJobToList(selected);
 		}
@@ -40,24 +41,27 @@ public class JobGenerator {
 	}
 	
 	/*
-	 * Used to generate a single job for all 6 characters
+	 * Used to generate a single job for all 6 characters Job1(same)/(No Job2)
 	 * @param: no parameters
-	 * @return selectedOne: the job that every character must be
+	 * @return selectedOne: returns a single job
 	 */
 	public Jobs oneJobEveryone() {
+		//randomly selects a single job
 		Jobs selectedOne = jobs[new Random().nextInt(jobs.length)];
 		
 		return selectedOne;
 	}
 	
 	/*
-	 * Used to generate two jobs that will be used by everyone
+	 * Used to generate two jobs that will be used by everyone Job1(Same)/Job2(Same)
 	 * @param: no parameters
-	 * @return bothJobs: a list with the two jobs that everyone must be job1/job2 for the entire game
+	 * @return bothJobs: a list with the two jobs
 	 */
 	public List<Jobs> oneJobEveryoneBoth() {
+		//List to hold the jobs
 		ArrayList<Jobs> bothJobs = new ArrayList<>();
 		
+		//while loop that only adds two jobs to the list.
 		while(bothJobs.size() < 2) {
 			twoJobsWholeParty(bothJobs);
 		}
@@ -67,58 +71,95 @@ public class JobGenerator {
 	
 	/*
 	 * Generates a 2D ArrayList of type Jobs where every character has a different job1, and job2
-	 * Which character gets which job will be random in the order they become permanent party members
+	 * Job1(Unique)/Job2(Unique)
 	 * @param: no parameters
 	 * @return twelveFilled: 2D ArrayList listing the order of job1/job2 for each member
 	 */
 	public ArrayList<ArrayList<Jobs>> differentBothJobs() {
+		//A 2D ArrayList of type Jobs
 		ArrayList<ArrayList<Jobs>> twelveFilled = new ArrayList<>();
 		
+		//Each row in the List will have its own ArrayList of type Jobs
 		twelveFilled.add(new ArrayList<Jobs>());
 		twelveFilled.add(new ArrayList<Jobs>());
 		
+		//loops through the two rows
 		for (int i = 0; i < twelveFilled.size(); i++) {
 			
+			//loops six times total to get a unique job each time.
 			for (int j = 0; j < 6; j++) {
-				//Jobs temp = jobs[new Random().nextInt(jobs.length)];
 				addJobToList(twelveFilled, i);
 			}
 		}
 		return twelveFilled;
 	}
 	
-	//every character has different job1s but same Job2
-	public ArrayList<ArrayList<Jobs>> differentJobTwo() {
-		
-	}
-	
-	//every character different job2s but same Job1
+	/*
+	 * Generates a 2D ArrayList of type Jobs where row 1 will have six unique jobs but row 2 will have one job
+	 * Job1 will be unique for each party member but Job2 will be the same for all six characters Job1(Unique)/Job2(same)
+	 * @param: no parameters
+	 * @return sevenDifferentJobOne: 2D ArrayList with the jobs generated for each member.
+	 */
 	public ArrayList<ArrayList<Jobs>> differentJobOne() {
+		ArrayList<ArrayList<Jobs>> sevenDifferentJobOne = new ArrayList<>();
 		
+		sevenDifferentJobOne.add(new ArrayList<Jobs>());
+		sevenDifferentJobOne.add(new ArrayList<Jobs>());
+		
+		//Generates a Job to insert into row at index 1 that is the Job2 that will be shared by everyone
+		sevenDifferentJobOne.get(1).add(jobs[new Random().nextInt(jobs.length)]);
+		
+		//this for loop will only add to row 0 of sevenDifferentJobOne
+		for (int i = 0; i < 1; i++) {
+			for (int j = 0; j < 6; j++) {
+				addJobToList(sevenDifferentJobOne, i);
+			}
+		}
+		
+		return sevenDifferentJobOne;
 	}
 	
-	
+	/*
+	 * Generates a 2D ArrayList of type Jobs where row 1 will be the same job but row 2 will be six unique jobs
+	 * Job2 will be unique but Job1 will be the same for all. Job1(Same)/Job2(Unique)
+	 * @param: no parameters
+	 * @return sevenDifferentJobTwo: 2D ArrayList with the jobs generated for each member
+	 */
+	public ArrayList<ArrayList<Jobs>> differentJobTwo() {
+		ArrayList<ArrayList<Jobs>> sevenDifferentJobTwo = new ArrayList<>();
+		
+		sevenDifferentJobTwo.add(new ArrayList<Jobs>());
+		sevenDifferentJobTwo.add(new ArrayList<Jobs>());
+		
+		//Generates a Job to insert into row at index 0 that is the Job1 that will be shared by everyone.
+		sevenDifferentJobTwo.get(0).add(jobs[new Random().nextInt(jobs.length)]);
+		
+		//this for loop will only add to row 1 of sevenDifferentJobTwo
+		for (int i = 1; i < 2; i++) {
+			for (int j = 0; j < 6; j++) {
+				addJobToList(sevenDifferentJobTwo, i);
+			}
+		}
+		
+		return sevenDifferentJobTwo;
+	}
 	
 	
 	//helper methods
 	
 	/*
-	 * Helper method to generate 6 different jobs and add to a list
-	 * @param aJobs: list where the jobs will be added
-	 * @return: no returns. Just used to add elements to list
+	 * Method to generate two jobs 
+	 * @param jobsList: list of Jobs that needs to have elements added to it
+	 * @return: no returns. Only adds the job to the list
 	 */
-	private void generateSixUniqueJobs(List<Jobs> aJobs) {
-		
+	private void twoJobsWholeParty(ArrayList<Jobs> jobsList) {
 		Jobs potentialJob = jobs[new Random().nextInt(jobs.length)];
 		
-		while (checkIfAlreadySelected(potentialJob, aJobs) == true) {
-			
+		while (checkIfAlreadySelected(potentialJob, jobsList) == true) {
+			potentialJob = jobs[new Random().nextInt(jobs.length)];
 		}
-	}
-	
-	//helper method for adding just 2 jobs
-	private void twoJobsWholeParty(ArrayList<Jobs> jobsList) {
 		
+		jobsList.add(potentialJob);
 	}
 	
 	/*
