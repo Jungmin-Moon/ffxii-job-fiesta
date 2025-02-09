023e957d12df1f4058b0b4edd4a47e288cb98d70
Jungmin Moon <jmmoon96@gmail.com>
2025-02-09 02:49:54 -0500

    Removed useless comments in User class

--- FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/entities/Runs.java
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/entities/Runs.java
@@ -1,16 +1,38 @@
 package com.zodiacfiesta.entities;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "runs")
 public class Runs {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long runID;
 	
 	private long userID;
 	private String runType;
-	private String vaanJobOne;
+	private Jobs vaanJobOne;
+	private Jobs vaanJobTwo;
+	private Jobs peneloJobOne;
+	private Jobs peneloJobTwo;
+	private Jobs franJobOne;
+	private Jobs franJobTwo;
+	private Jobs balthierJobone;
+	private Jobs balthierJobTwo;
+	private Jobs asheJobOne;
+	private Jobs asheJobTwo;
+	private Jobs baschJobOne;
+	private Jobs baschJobTwo;
 	
+	private LocalDateTime dateStarted;
+	private LocalDateTime dateFinished;
+	
+	private boolean runFinished;
 }
--- FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/entities/User.java
+++ FFXII-Job-Fiesta/src/main/java/com/zodiacfiesta/entities/User.java
@@ -1,5 +1,6 @@
 package com.zodiacfiesta.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,80 +19,42 @@ public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "userID")
 	private long id;
 	
 	private String username;
 	private String password;
 	private String role;
 	
-	/*
-	 * Fetches the id of the user from the db
-	 * @param: no parameters
-	 * @return: the user id
-	 */
+	
 	public long getId() {
 		return id;
 	}
 	
-	/*
-	 * Sets the user id
-	 * @param id: the id to store for this particular user
-	 * @return: no return
-	 */
 	public void setId(long id) {
 		this.id = id;
 	}
 	
-	/*
-	 * Fetches the username of the user from the db
-	 * @param: no parameters
-	 * @return: the username of the user
-	 */
 	public String getUsername() {
 		return username;
 	}
 	
-	/*
-	 * Sets the username for this user
-	 * @param username: the username to store into the field value for this value
-	 * @return: no return
-	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 	
-	/*
-	 * Fetches the user password from the db
-	 * @param: no parameters
-	 * @return: the user's password
-	 */
 	public String getPassword() {
 		return password;
 	}
 	
-	/*
-	 * Sets the user's password
-	 * @param password: the password to store into the field password
-	 * @return: no returns
-	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
-	/*
-	 * Fetches the user's role that is stored in the db
-	 * @param: no parameters
-	 * @return: the user's role
-	 */
 	public String getRole() {
 		return role;
 	}
 	
-	/*
-	 * Sets the user's role
-	 * @param role: the role to store into the field value role
-	 * @return: no returns
-	 */
 	public void setRole(String role) {
 		this.role = role;
 	}
